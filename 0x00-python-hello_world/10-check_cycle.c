80c2508a00a9079ceb80c1603815e14f9dba57cd
kmurimi-devs <kmurimi.devs@gmail.com>
2023-08-11 13:57:57 +0000

    linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include <list.h>
+/**
+ *check_cycle - check if linked list contains a cycle
+ *@list: linked list
+ *return: 0 for no loop, 1 for looped
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+	
+	if (!list)
+		return (0);
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
