e800ebcdb82cb20990245b144afc600fd75f020b
kmurimi-devs <kmurimi.devs@gmail.com>
2023-08-17 19:20:56 +0000

    combo fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for first in range(0, 10):
         if first == 8 and second == 9:
             print("{}{}".format(first, second))
         else:
-            print("{}{}".format(first, second),end=", ")
+            print("{}{}".format(first, second), end=", ")
