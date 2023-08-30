0f9d3d3aa94cbbfbd2946d379744660c8e99e6ae
kmurimi-devs <kmurimi.devs@gmail.com>
2023-08-30 21:37:32 +0000

    private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Define class of Square"""
+
+
+class Square:
+    """ Rep a square"""
+
+    def __init__(self, size):
+        """initialize new square"""
+
+        self.__size = size
