a725e0b1e6fc80c8973e3615a4f8ef9ca76d069e
kmurimi-devs <kmurimi.devs@gmail.com>
2023-10-25 17:26:29 +0000

    bug fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = my_list[0]
     if len(my_list) == 0:
         return None
     else:
+        max = my_list[0]
         for num in my_list:
             if num > max:
                 max = num
