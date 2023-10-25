53f5f8887d561583b422cfc61f6976ea1a0d970b
kmurimi-devs <kmurimi.devs@gmail.com>
2023-10-25 16:55:16 +0000

    format match

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{:d} ".format(num), end="")
+            print("{:d}".format(num), end=" " if col != row[-1] else "")
         print()
