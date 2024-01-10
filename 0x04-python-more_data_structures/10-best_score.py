33b8cb7c8279c3267a824433b9e56b0e021718bc
kmurimi-devs <kmurimi.devs@gmail.com>
2024-01-10 09:12:52 +0000

    best score null handling

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_score = max(a_dictionary, key=lambda k: a_dictionary[k])
-    return max_score
+    if a_dictionary:
+        max_score = max(a_dictionary, key=lambda k: a_dictionary[k])
+        return max_score
+    else:
+        return None
